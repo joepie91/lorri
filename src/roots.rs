5b8c112a85c7fe4c081671afdf0c6ce9da412942
Graham Christensen <graham@grahamc.com>
2019-04-17 21:36:27 -0400

    roots: consider NIX_STATE_DIR

--- src/roots.rs
+++ src/roots.rs
@@ -35,8 +35,15 @@ impl Roots {
         ignore_missing(std::fs::remove_file(&path))?;
         symlink(&store_path, &path)?;
 
-        // this is bad.
-        let mut root = PathBuf::from("/nix/var/nix/gcroots/per-user");
+        let mut root = if let Ok(path) = env::var("NIX_STATE_DIR") {
+            PathBuf::from(path)
+        } else {
+            PathBuf::from("/nix/var/nix/")
+        };
+        root.push("gcroots");
+        root.push("per-user");
+
+        // TODO: check on start
         root.push(env::var("USER").expect("env var 'USER' must be set"));
         root.push(format!("{}-{}", self.id, name));
 
